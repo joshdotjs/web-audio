474fa698e50a8f724f180bd168c7a30642939d81
joshDotJS <joshDotJS@gmail.com>
2023-07-01 09:48:59 -0500

    effect

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const qs = str => document.querySelector(str);
 
-const synth = new Tone.Synth().toDestination();
 
-const play_btn = qs('#play-btn');
-play_btn.addEventListener('click', () => {
-  console.log('clicked');
-  if (Tone.context.state !== 'running') {
-    Tone.start();
-  } 
+// ==============================================
 
-  synth.triggerAttackRelease('C4', '2n');
-});
+const playNote = () => {
+  const synth = new Tone.Synth().toDestination();
+
+  const play_btn = qs('#play-note-btn');
+  play_btn.addEventListener('click', () => {
+    console.log('play note');
+    if (Tone.context.state !== 'running') {
+      Tone.start();
+    } 
+    synth.triggerAttackRelease('C4', '2n');
+  });
+};
+playNote();
+
+// ==============================================
+
+const playEffect = () => {
+  const synth = new Tone.Synth();
+  const feedback_delay = new Tone.FeedbackDelay(
+    '8n.',
+    0.6
+  );
+  synth.connect(feedback_delay);
+  feedback_delay.toDestination();
+
+  const play_btn = qs('#play-effect-btn');
+  play_btn.addEventListener('click', () => {
+    console.log('play effect');
+    if (Tone.context.state !== 'running') {
+      Tone.start();
+    } 
+
+    synth.triggerAttackRelease('C4', '2n');
+  });
+};
+playEffect();
+
+// ==============================================
